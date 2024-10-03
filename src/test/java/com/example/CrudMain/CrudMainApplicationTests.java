0781160a34cbf17b7a060c36370beae181a7c74e
RAHUL KUMAR M S <rahulmanakyalath@gmail.com>
2024-10-03 15:48:59 +0530

    initial commit

---
+++ src/test/java/com/example/CrudMain/CrudMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CrudMain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
